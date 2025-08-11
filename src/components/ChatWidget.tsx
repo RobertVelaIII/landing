99642ba98b76f2751088d16965b8c121db900437
Robert Vela <xssistant@icloud.com>
2025-08-11 03:28:46 -0500

    Update ChatWidget.tsx

--- src/components/ChatWidget.tsx
+++ src/components/ChatWidget.tsx
@@ -134,7 +134,7 @@ export default function ChatWidget() {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z" />
               </svg>
             </div>
-            <p className="font-medium">Need help?</p>
+            <p className="font-medium text-gray-900 text-lg">Need help?</p>
           </div>
           <p className="text-sm text-gray-700">Speak to Chrispy Craig 24/7</p>
           <button 
